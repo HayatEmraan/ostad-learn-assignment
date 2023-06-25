ee4f674772bde5157f8d9826ffe8f2ecb47005cd
hayatemraan <emraanhasanhayat@gmail.com>
2023-06-25 12:40:34 +0600

    date added

---
+++ date.js
@@ -0,0 +1,25 @@
+function myDateFunc(dateNew) {
+  var daysNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let d = new Date(dateNew);
+  let day = d.getDate();
+  let month = monthNames[d.getMonth()];
+  let year = d.getFullYear();
+  let dayName = daysNames[d.getDay()];
+  console.log(dayName + " " + day + " " + month + " " + year);
+}
+
+myDateFunc("2023-11-25");
---
+++ function.js
@@ -0,0 +1,26 @@
+// Regular function
+
+// function myNewFunc() {
+//     console.log("Regular Func Called");
+// }
+
+// myNewFunc();
+
+
+// function myTwoFunc(n1 = 0, n2 = 0) {
+//     console.log(n1 + n2);
+// }
+
+// myTwoFunc(10, 20);
+
+
+// Function Expression
+
+// const arrowNext = (num2, num3) => {
+//     console.log(num2 + num3);
+// }
+
+
+// arrowNext(2, 23);
+
+
